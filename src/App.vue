b572b210e29edc392978fcf023c2e76c2b48b85e
tuan.pham <tuananh.pham@xtel.vn>
2019-10-08 16:33:15 +0700

    ví dụ về slot của VueJs

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <app-card cartWords="Meow">
+            <h2 slot="title">{{cardTitle}}</h2>
+            <p>white Cat is so noob!</p>
+          </app-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    import Card from "./components/Card.vue";
+
+    export default {
+        components: {
+            appCard: Card
+        },
+        data: () => ({
+            cardTitle: 'White Cat'
+        })
+    };
+</script>
+
+<style>
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="title">
+      <slot name="title"></slot>
+      <span>
+<!--        slot have value default which is slot default-->
+        <slot name="subTitles">The SubTitles</slot>
+      </span>
+    </div>
+    <div class="content">
+      <!--slot unsign is default slot-->
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ["cartWords"]
+    };
+</script>
+
+<style scoped>
+  div {
+    widows: 100;
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.6);
+    padding: 30px;
+    margin: 20px auto;
+    text-align: center;
+  }
+
+  h2 {
+    color: red;
+  }
+</style>
